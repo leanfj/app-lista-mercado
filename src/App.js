0191714a3bbf05ddd9918bd801166c6132e37f2e
leanfj <leanfj@hotmail.com>
2018-07-24 17:33:10 -0300

    Finalizado updateItem

--- src/App.js
+++ src/App.js
@@ -1,92 +1,122 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 //Components
-import Modal from './components/Modal';
+import Modal from "./components/Modal";
 
-
-const dados = (localStorage.getItem('lista') ? JSON.parse(localStorage.getItem('lista')) : []);
+const dados = localStorage.getItem("lista")
+  ? JSON.parse(localStorage.getItem("lista"))
+  : [];
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       items: [],
-      modalShow: false
+      modalShow: false,
+      itemIndex: "",
+      itemName: "",
+      itemQuant: "",
+      itemPrice: ""
     };
 
     this.renderItems = this.renderItems.bind(this);
     this.addNewItem = this.addNewItem.bind(this);
     this.removeItem = this.removeItem.bind(this);
     this.closeModal = this.closeModal.bind(this);
-  }
-  
-  componentDidMount() {
-    this.setState({
-      items: JSON.parse(localStorage.getItem('lista'))
-    }
-    );
+    this.updateItem = this.updateItem.bind(this);
   }
 
-  renderItems (argItem, argIndex) {
+  componentDidMount() {
+    this.setState({
+      items: JSON.parse(localStorage.getItem("lista"))
+    });
+  }
+
+  renderItems(argItem, argIndex) {
     return (
       <tr key={argIndex}>
         <td>{argItem.item}</td>
         <td>{argItem.quantidade}</td>
         <td>R$ {argItem.valorUnitario}</td>
         <td>R$ {argItem.valorTotal}</td>
-        <td onClick={() => this.removeItem(argIndex)} ><i className="fas fa-trash-alt"></i></td>
-        <td onClick={() => this.openModal()} ><i className="fas fa-pencil-alt"></i></td>
+        <td onClick={() => this.removeItem(argIndex)}>
+          <i className="fas fa-trash-alt" />
+        </td>
+        <td
+          onClick={() =>
+            this.openModal(
+              argIndex,
+              argItem.item,
+              argItem.quantidade,
+              argItem.valorUnitario
+            )
+          }
+        >
+          <i className="fas fa-pencil-alt" />
+        </td>
       </tr>
-
-    )
+    );
   }
 
-  openModal () {
-    console.log('openModal');
-    this.setState({modalShow: true});
-    
-
+  openModal(index, item, quant, price) {
+    this.setState({
+      modalShow: true,
+      itemIndex: index,
+      itemName: item,
+      itemQuant: quant,
+      itemPrice: price
+    });
   }
 
-  closeModal () {
-    this.setState({modalShow: false});
-    console.log('closeModal');
+  closeModal() {
+    this.setState({ modalShow: false });
   }
 
-  addNewItem () {
+  addNewItem() {
     const newItem = {
       item: this.refs.item.value,
       quantidade: this.refs.quantidade.value,
       valorUnitario: this.refs.valorUnitario.value,
-      valorTotal: (this.refs.valorUnitario.value * this.refs.quantidade.value).toFixed(2),
-    }
+      valorTotal: (
+        this.refs.valorUnitario.value * this.refs.quantidade.value
+      ).toFixed(2)
+    };
 
     dados.push(newItem);
-    localStorage.setItem('lista', JSON.stringify(dados));
+    localStorage.setItem("lista", JSON.stringify(dados));
     this.setState({
-      items: JSON.parse(localStorage.getItem('lista'))
+      items: JSON.parse(localStorage.getItem("lista"))
     });
 
-    this.refs.item.value = '';
-    this.refs.quantidade.value = '';
-    this.refs.valorUnitario.value = '';
+    this.refs.item.value = "";
+    this.refs.quantidade.value = "";
+    this.refs.valorUnitario.value = "";
   }
 
-  removeItem (argIndex) {
+  removeItem(argIndex) {
     dados.splice(argIndex, 1);
-    localStorage.setItem('lista', JSON.stringify(dados));
+    localStorage.setItem("lista", JSON.stringify(dados));
     this.setState({
-      items: JSON.parse(localStorage.getItem('lista'))
+      items: JSON.parse(localStorage.getItem("lista"))
     });
   }
-  
-
+  updateItem(index, quant, price) {
+    console.log(dados);
+    dados[index].quantidade = quant;
+    dados[index].valorUnitario = price;
+    dados[index].valorTotal = (quant * price).toFixed(2);
+    localStorage.setItem("lista", JSON.stringify(dados));
+    this.setState({
+      items: JSON.parse(localStorage.getItem("lista"))
+    });
+    this.closeModal();
+  }
   render() {
     let totalCompra = 0;
     if (this.state.items) {
-      this.state.items.map((item) => {
-        return totalCompra += parseFloat(item.valorTotal);
+      this.state.items.map(item => {
+        return (totalCompra += parseFloat(item.valorTotal));
       });
     }
 
@@ -100,33 +130,64 @@ class App extends Component {
               <th>Un./Peso</th>
               <th>Valor Un.</th>
               <th>Valor Total</th>
-              <th></th>
-              <th></th>
+              <th />
+              <th />
             </tr>
           </thead>
           <tbody>
-            {this.state.items ? this.state.items.map(this.renderItems) : ''}
+            {this.state.items ? this.state.items.map(this.renderItems) : ""}
           </tbody>
         </table>
         <div className="total__compra text-light bg-dark d-flex justify-content-center">
           Total Compra R$ {totalCompra.toFixed(2)}
         </div>
-        <Modal show={this.state.modalShow} closeModal={this.closeModal} />
+        <Modal
+          show={this.state.modalShow}
+          itemIndex={this.state.itemIndex}
+          itemName={this.state.itemName}
+          itemQuant={this.state.itemQuant}
+          itemPrice={this.state.itemPrice}
+          closeModal={this.closeModal}
+          updateItem={this.updateItem}
+        />
         <div className="add-item">
           <h5 className="text-center">Adcionar novo item</h5>
           <form action="">
             <div className="form-group">
-              <input className="form-control" type="text" ref="item" placeholder="Item" />
+              <input
+                className="form-control"
+                type="text"
+                ref="item"
+                placeholder="Item"
+              />
             </div>
             <div className="row">
               <div className="form-group col-sm-6">
-                <input className="form-control" type="number" ref="quantidade" placeholder="Quantidade/Peso" />
+                <input
+                  className="form-control"
+                  type="number"
+                  ref="quantidade"
+                  placeholder="Quantidade/Peso"
+                />
               </div>
               <div className="form-group col-sm-6">
-                <input className="form-control" type="number" ref="valorUnitario" placeholder="Valor Un." />
+                <input
+                  className="form-control"
+                  type="number"
+                  ref="valorUnitario"
+                  placeholder="Valor Un."
+                />
               </div>
             </div>
-            <button type="button" value="submit" onClick={this.addNewItem}className="btn btn-primary"> Adcionar</button>
+            <button
+              type="button"
+              value="submit"
+              onClick={this.addNewItem}
+              className="btn btn-primary"
+            >
+              {" "}
+              Adcionar
+            </button>
           </form>
         </div>
       </div>
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,33 +1,82 @@
-import React, { Component } from 'react';
-import './modal.css';
+import React, { Component } from "react";
+import "./modal.css";
 
 class Modal extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      display: 'block'
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
-  render () {
+
+  render() {
     const showModal = {
-      display: 'block'
-    }
+      display: "block"
+    };
     return (
-      <div className="modal" role="dialog" style={ this.props.show ? showModal : {}}>
+      <div
+        className="modal"
+        role="dialog"
+        style={this.props.show ? showModal : {}}
+      >
         <div className="modal-dialog" role="document">
           <div className="modal-content">
             <div className="modal-header">
-              <h5 className="modal-title">Modal title</h5>
-              <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={() => this.props.closeModal()}>
+              <h5 className="modal-title">{this.props.itemName}</h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+                onClick={() => this.props.closeModal()}
+              >
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div className="modal-body">
-              <p>Modal body text goes here.</p>
+              <form action="">
+                <div className="row">
+                  <div className="form-group col-sm-6">
+                    <input
+                      className="form-control"
+                      type="number"
+                      placeholder="Quantidade/Peso"
+                      ref="quantidade"
+                      defaultValue={this.props.itemQuant}
+                    />
+                  </div>
+                  <div className="form-group col-sm-6">
+                    <input
+                      className="form-control"
+                      type="number"
+                      placeholder="Valor Un."
+                      ref="preco"
+                      defaultValue={this.props.itemPrice}
+                    />
+                  </div>
+                </div>
+              </form>
             </div>
             <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={() => this.props.closeModal()}>Close</button>
-              <button type="button" className="btn btn-primary">Save changes</button>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+                onClick={() => this.props.closeModal()}
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={() =>
+                  this.props.updateItem(
+                    this.props.itemIndex,
+                    this.refs.quantidade.value,
+                    this.refs.preco.value
+                  )
+                }
+              >
+                Save changes
+              </button>
             </div>
           </div>
         </div>
